4609a13873b85cae8cda75ce22332ce3aba50783
Bani Faza Rasyadan <banifr@Banis-MacBook-Pro.local>
2023-07-09 21:37:18 +0700

    Initial commit

---
+++ app/src/main/java/com/example/a10119110_mvp/MainPresenter.java
@@ -0,0 +1,26 @@
+package com.example.a10119110_mvp;
+
+public class MainPresenter {
+
+    private MainView view;
+
+    public MainPresenter(MainView view) {
+        this.view = view;
+    }
+
+    public void calculateLength(String meter) {
+        if (meter.isEmpty())
+            meter = "0";
+
+        Meter model = new Meter();
+        double parsedMeter = Double.parseDouble(meter);
+        model.setMeter(parsedMeter);
+
+        String kilometer = model.toKilometer() + "";
+        String  centimeter = model.toCentimeter() + "";
+
+        view.showKilometer(kilometer);
+        view.showCentimeter(centimeter);
+    }
+
+}
---
+++ app/src/main/java/com/example/a10119110_mvp/MainView.java
@@ -0,0 +1,7 @@
+package com.example.a10119110_mvp;
+
+public interface MainView {
+    void showKilometer(String kilometer);
+
+    void showCentimeter(String centimeter);
+}
